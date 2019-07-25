f7677a9e308c30ea6f5cef2e029289246cdcd0fe
Yoshio Hasegawa <yoshio.hasegawa@pitchbook.com>
2019-07-25 13:25:44 -0700

    Updating setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # unbounceapi/setup.py
-from distutils.core import setup
+from setuptools import setup
 
 with open("README.md", "r") as f:
     long_description = f.read()
@@ -7,15 +7,15 @@ with open("README.md", "r") as f:
 setup(
   name = 'unbounce-python-api',
   packages = ['unbounceapi'],
-  version = '1.1.3',
+  version = '1.1.4',
   license='MIT',
   description = 'An Unbounce API wrapper written in python.',
   long_description = long_description,
-  long_description_content_type = "text/markdown",
+  long_description_content_type = 'text/markdown',
   author = 'Yoshio Hasegawa',
   author_email = 'yoshiohasegawa206@gmail.com',
   url = 'https://github.com/YoshioHasegawa/unbounce-python-api',
-  download_url = 'https://github.com/YoshioHasegawa/unbounce-python-api/archive/v1.1.3.tar.gz',
+  download_url = 'https://github.com/YoshioHasegawa/unbounce-python-api/archive/v1.1.4.tar.gz',
   keywords = ['Unbounce', 'API', 'Wrapper'],
   install_requires=[
           'requests',
